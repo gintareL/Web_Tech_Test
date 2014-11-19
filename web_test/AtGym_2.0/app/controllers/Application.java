62facf089109578d54a6e25d7fce8945053f6e35
gintareL <gintare.lideikyte@gmail.com>
2014-11-19 09:02:22 +0100

    update ourgym

--- web_test/AtGym_2.0/app/controllers/Application.java
+++ web_test/AtGym_2.0/app/controllers/Application.java
@@ -19,5 +19,13 @@ public class Application extends Controller {
 	public static Result anmeldung(){
 		return ok(anmeldung.render());
 	}
+	
+	public static Result ourgym(){
+		return ok(our_gym_boot.render());
+	}
+	
+	public static Result mygym(){
+		return ok(myGym.render());
+	}
 
 }
