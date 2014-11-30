61fea730fde7f2d7d9d6986ba9d8ad25e90dadee
gintareL <gintare.lideikyte@gmail.com>
2014-11-30 13:24:29 +0100

    listen mit tabs

--- web_test/AtGym_2.0/AtGym/app/models/Plan.java
+++
@@ -1,7 +0,0 @@
-package models;
-import java.util.*;
-
-public class Plan{
-	private String name;
-	private Map<Tag, HashSet<Uebung>> uebungen;
-}
--- web_test/AtGym_2.0/AtGym/app/models/Tag.java
+++
@@ -1,5 +0,0 @@
-package models;
-
-public enum Tag {
-	Monntag, Dienstag, Mittwoch, Donnerstag, Freitag, Samstag, Sonntag
-}
